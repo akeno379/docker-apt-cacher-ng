215736cbbae782851a75c0657a19a081db448df6
seifer08ms <hi181904665@gmail.com>
2017-02-14 15:18:44 +0800

    add proxy to apt-cacher-ng,and more passthough rules

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 sed 's/# ForeGround: 0/ForeGround: 1/' -i /etc/apt-cacher-ng/acng.conf \
- && echo 'PassThroughPattern: (packages-gitlab-com\.s3\.amazonaws\.com|packages-gitlab-com\.s3-accelerate\.amazonaws\.com|packages\.gitlab\.com|mirrors\.fedoraproject\.org):443 | get\.docker\.com|download\.oracle\.com' >> /etc/apt-cacher-ng/acng.conf \
- && echo 'VfilePatternEx: ^(/\?release=[0-9]+&arch=.*)$' >>  /etc/apt-cacher-ng/acng.conf \
+ && echo 'PassThroughPattern: (packages-gitlab-com\.s3\.amazonaws\.com|packages-gitlab-com\.s3-accelerate\.amazonaws\.com|packages\.gitlab\.com|mirrors\.fedoraproject\.org):443 | get\.docker\.com|download\.oracle\.com| archive\.ubuntukylin\.com:10006|httpredir\.debian\.org' >> /etc/apt-cacher-ng/acng.conf \
+ && echo 'VfilePatternEx: ^(/\?release=[0-9]+&arch=.*)$|(^|.*?/)(Index|Packages\.bz2|Packages\.gz|Packages|Release|Release\.gpg|Sources\.bz2|Sources\.gz|Sources|release|index\.db-.*\.gz|Contents-[^/]*\.gz|pkglist[^/]*\.bz2|rclist[^/]*\.bz2|/meta-release[^/]*|Translation[^/]*\.bz2|.*\.db)$' >>  /etc/apt-cacher-ng/acng.conf \
  && echo 'Remap-centos: file:centos_mirrors ; http://mirror.ox.ac.uk/sites/mirror.centos.org/' >>  /etc/apt-cacher-ng/acng.conf \
- && echo 'PrecacheFor: */dists/* ' >> /etc/apt-cacher-ng/acng.conf \
+ && echo 'PrecacheFor: uburep/dists/*  debrep/dists/*/main/binary-amd64/* ' >> /etc/apt-cacher-ng/acng.conf \
+ && echo 'Proxy: http://xx-net:8087 ' >> /etc/apt-cacher-ng/acng.conf \
+ && echo 'OptProxyCheckInterval: 60 ' >> /etc/apt-cacher-ng/acng.conf \
+ && echo 'OptProxyTimeout: 3 ' >> /etc/apt-cacher-ng/acng.conf \
  && echo 'Offlinemode : 0' >>  /etc/apt-cacher-ng/acng.conf 
+
+echo 'Acquire::http { Proxy "http://localhost:3142"; };' | tee /etc/apt/apt.conf.d/02proxy
